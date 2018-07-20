e8f7ffd0fb13c070ff2b1bb52d9d13971a93a83e
ibra.z <ibra.z@kakaocorp.com>
2018-07-20 13:34:40 +0900

    1장 - 시작하기, Router적용

---
+++ src/contailner/Main.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Main extends Component{
+    render(){
+        return (
+            <div>Main</div>
+        )
+    }
+}
+
+export default Main;
---
+++ src/contailner/Viewer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Viewer extends Component{
+    render(){
+        return (
+            <div>Viewer</div>
+        )
+    }
+}
+
+export default Viewer;
